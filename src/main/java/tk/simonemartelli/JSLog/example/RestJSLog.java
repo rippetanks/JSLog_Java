5d43563fd3a1d12bc84567a2c0ebcaab3100f673
rippetanks <simone.martelli.98@gmail.com>
2019-01-04 21:54:55 +0100

    Bug fix

--- src/main/java/tk/simonemartelli/JSLog/example/RestJSLog.java
+++ src/main/java/tk/simonemartelli/JSLog/example/RestJSLog.java
@@ -8,6 +8,7 @@ package tk.simonemartelli.JSLog.example;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,72 +32,84 @@ public class RestJSLog {
     public void addMongoDB(@RequestBody String json) throws JSONException, 
             ParseException, SQLException 
     {
-        JSONObject jsonObj = new JSONObject(json);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        JSLogObject obj = new JSLogObject(
-            jsonObj.optString("entity"),
-            format.parse(jsonObj.getString("record_date")),
-            jsonObj.getString("level"),
-            jsonObj.optString("UserAgent"),
-            jsonObj.optString("Host"),
-            jsonObj.getString("message"),
-            jsonObj.optInt("http_code"));
-        JSLog log = JSLog.getInstance();
-        log.add("KEY", obj);
+        JSONArray array = new JSONArray(json);
+        for(int i = 0; i < array.length(); i++) {
+            JSONObject jsonObj = array.getJSONObject(i);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            JSLogObject obj = new JSLogObject(
+                jsonObj.optString("entity"),
+                format.parse(jsonObj.getString("record_date")),
+                jsonObj.getString("level"),
+                jsonObj.optString("UserAgent"),
+                jsonObj.optString("Host"),
+                jsonObj.getString("message"),
+                jsonObj.optInt("http_code"));
+            JSLog log = JSLog.getInstance();
+            log.add("KEY", obj);
+        }
     }
     
     @RequestMapping(value="/JSLog/log/mysql", method=RequestMethod.POST)
     public void addMySQL(@RequestBody String json) throws JSONException, 
             ParseException, SQLException 
     {
-        JSONObject jsonObj = new JSONObject(json);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        JSLogObject obj = new JSLogObject(
-            jsonObj.optInt("entity"),
-            format.parse(jsonObj.getString("record_date")),
-            jsonObj.getString("level"),
-            jsonObj.optString("UserAgent"),
-            jsonObj.optString("Host"),
-            jsonObj.getString("message"),
-            jsonObj.optInt("http_code"));
-        JSLog log = JSLog.getInstance();
-        log.add("KEY", obj);
+        JSONArray array = new JSONArray(json);
+        for(int i = 0; i < array.length(); i++) {
+            JSONObject jsonObj = array.getJSONObject(i);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            JSLogObject obj = new JSLogObject(
+                jsonObj.optInt("entity"),
+                format.parse(jsonObj.getString("record_date")),
+                jsonObj.getString("level"),
+                jsonObj.optString("UserAgent"),
+                jsonObj.optString("Host"),
+                jsonObj.getString("message"),
+                jsonObj.optInt("http_code"));
+            JSLog log = JSLog.getInstance();
+            log.add("KEY", obj);
+        }
     }
     
     @RequestMapping(value="/JSLog/log/sqlserver", method=RequestMethod.POST)
     public void addSQLServer(@RequestBody String json) throws JSONException, 
             ParseException, SQLException 
     {
-        JSONObject jsonObj = new JSONObject(json);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        JSLogObject obj = new JSLogObject(
-            jsonObj.optInt("entity"),
-            format.parse(jsonObj.getString("record_date")),
-            jsonObj.getString("level"),
-            jsonObj.optString("UserAgent"),
-            jsonObj.optString("Host"),
-            jsonObj.getString("message"),
-            jsonObj.optInt("http_code"));
-        JSLog log = JSLog.getInstance();
-        log.add("KEY", obj);
+        JSONArray array = new JSONArray(json);
+        for(int i = 0; i < array.length(); i++) {
+            JSONObject jsonObj = array.getJSONObject(i);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            JSLogObject obj = new JSLogObject(
+                jsonObj.optInt("entity"),
+                format.parse(jsonObj.getString("record_date")),
+                jsonObj.getString("level"),
+                jsonObj.optString("UserAgent"),
+                jsonObj.optString("Host"),
+                jsonObj.getString("message"),
+                jsonObj.optInt("http_code"));
+            JSLog log = JSLog.getInstance();
+            log.add("KEY", obj);
+        }
     }
     
     @RequestMapping(value="/JSLog/log/postgres", method=RequestMethod.POST)
     public void addPostgres(@RequestBody String json) throws JSONException, 
             ParseException, SQLException 
     {
-        JSONObject jsonObj = new JSONObject(json);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        JSLogObject obj = new JSLogObject(
-            jsonObj.optInt("entity"),
-            format.parse(jsonObj.getString("record_date")),
-            jsonObj.getString("level"),
-            jsonObj.optString("UserAgent"),
-            jsonObj.optString("Host"),
-            jsonObj.getString("message"),
-            jsonObj.optInt("http_code"));
-        JSLog log = JSLog.getInstance();
-        log.add("KEY", obj);
+        JSONArray array = new JSONArray(json);
+        for(int i = 0; i < array.length(); i++) {
+            JSONObject jsonObj = array.getJSONObject(i);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            JSLogObject obj = new JSLogObject(
+                jsonObj.optInt("entity"),
+                format.parse(jsonObj.getString("record_date")),
+                jsonObj.getString("level"),
+                jsonObj.optString("UserAgent"),
+                jsonObj.optString("Host"),
+                jsonObj.getString("message"),
+                jsonObj.optInt("http_code"));
+            JSLog log = JSLog.getInstance();
+            log.add("KEY", obj);
+        }
     }
     
 }
